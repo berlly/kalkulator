4f291cf083fe4f12959837c6131e953bc83bdaee
berlly <berllyd3@gmail.com>
2018-06-25 13:36:11 +0700

    versi 1.1

---
+++ app/src/main/java/com/example/iptrans/kakulator/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.iptrans.kakulator;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements OnClickListener {
+    EditText vTxtA, vTxtB, vTxtC;
+
+    protected float yTxtA, yTxtB, yTxtC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vTxtA = (EditText) findViewById(R.id.txtA);
+        vTxtB = (EditText) findViewById(R.id.txtB);
+        vTxtC = (EditText) findViewById(R.id.txtC);
+
+        Button vBtnTambah = (Button) findViewById(R.id.btnTambah);
+        Button vBtnKurang = (Button) findViewById(R.id.btnKurang);
+        Button vBtnKali = (Button) findViewById(R.id.btnKali);
+        Button vBtnBagi = (Button) findViewById(R.id.btnBagi);
+
+        vBtnTambah.setOnClickListener(this);
+        vBtnKurang.setOnClickListener(this);
+        vBtnKali.setOnClickListener(this);
+        vBtnBagi.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        yTxtA= Float.parseFloat(vTxtA.getText().toString());
+        yTxtB= Float.parseFloat(vTxtB.getText().toString());
+
+        switch (v.getId()) {
+            case R.id.btnTambah:
+                yTxtC = yTxtA + yTxtB;
+
+                vTxtC.setText(Float.toString(yTxtC));
+            }
+        switch (v.getId()) {
+            case R.id.btnKurang:
+                yTxtC = yTxtA - yTxtB;
+
+                vTxtC.setText(Float.toString(yTxtC));
+            }
+        switch (v.getId()) {
+            case R.id.btnBagi:
+                yTxtC = yTxtA / yTxtB;
+
+                vTxtC.setText(Float.toString(yTxtC));
+            }
+        switch (v.getId()) {
+            case R.id.btnKali:
+                yTxtC = yTxtA * yTxtB;
+
+                vTxtC.setText(Float.toString(yTxtC));
+        }
+    }
+
+}
